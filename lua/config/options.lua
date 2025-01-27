92c4ddfb4b7e4a308d3f7848dc59bf76978328f6
Diego Schmidt <schmas+gh@pm.me>
2025-01-27 14:18:15 -0300

    Updated

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -45,3 +45,5 @@
 -- vim.opt.iskeyword:append '-' -- Hyphenated words recognized by searches (default: does not include '-')
 -- vim.opt.formatoptions:remove { 'c', 'r', 'o' } -- Don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode. (default: 'croql')
 -- vim.opt.runtimepath:remove '/usr/share/vim/vimfiles' -- Separate Vim plugins from Neovim in case Vim still in use (default: includes this path if Vim is installed)
+
+vim.g.snacks_animate = false
